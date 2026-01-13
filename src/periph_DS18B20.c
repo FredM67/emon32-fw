e14b8115068f6d77cd4218f31a94e98f99d61020
Angus Logan <awjlogan@gmail.com>
2026-01-13 20:41:06 +0000

    [OneWire] Address match must be valid DS18B20 address
      - Was matching 0 == 0, which is not valid, which meant that T1
        was mapped to T8.
      - Only remap when (uint8_t)address == 0x28

--- src/periph_DS18B20.c
+++ src/periph_DS18B20.c
@@ -325,7 +325,7 @@ void ds18b20MapSensors(const uint64_t *pAddr) {
   for (int i = 0; i < TEMP_MAX_ONEWIRE; i++) {
     devRemap[i] = i;
     for (int j = 0; j < TEMP_MAX_ONEWIRE; j++) {
-      if (devTableAddr[i] == pAddr[j]) {
+      if ((devTableAddr[i] == pAddr[j]) && (0x28 == (uint8_t)devTableAddr[i])) {
         devRemap[j] = devRemap[i];
         devRemap[i] = j;
         break;
